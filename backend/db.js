45a04252c0738efc837d042b3252868b4b88800d
Shikhaxingh01 <shikhasingh5931@gmail.com>
2024-04-14 12:47:35 +0530

    "Database connected"

---
+++ backend/db.js
@@ -0,0 +1,24 @@
+// const mongoose=require('mongoose');
+// const mongoURI= "mongodb://localhost:27017"
+// const connectToMongo=()=>{
+//     mongoose.connect(mongoURI,function () {
+//             console.log("Connected to mongodb Successfully");
+//         })
+// }
+// module.exports= connectToMongo;
+
+const mongoose = require("mongoose");
+const mongoURI= "mongodb://localhost:27017";
+
+const connectToMongo = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(mongoURI);
+    console.log("Connected to MongoDB Successfully!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = connectToMongo;
+
+
